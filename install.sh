7e31f8b39491e76d3366fe0516fbe34493bd25fb
Henrik Soerensen <henrik@voi.network>
2024-08-29 11:24:55 -0400

    Submission to chain works

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ network_status_url=""
 network_identifier=""
 staking_url=""
 container_id=""
+latest_key_end_block=""
 
 voi_logo="[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m
 [0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m[38;2;255;255;255mM[0m
@@ -527,7 +528,8 @@ generate_new_key() {
   end_block=$((start_block + 2000000))
   expiration_date=$(get_participation_expiration_eta "${end_block}" "${start_block}")
   address=$1
-  echo "Generating participation key for account ${address} with start block ${start_block} and end block ${end_block}"
+  latest_key_end_block=${end_block}
+  echo "Generating participation key for ${address} with start block ${start_block} and end block ${end_block}"
   echo "New key is expected to be valid until: ${expiration_date}"
   execute_interactive_docker_command "/node/bin/goal account addpartkey -a ${address} --roundFirstValid ${start_block} --roundLastValid ${end_block}"
 }
@@ -561,6 +563,48 @@ ensure_accounts_are_offline() {
       echo "Account ${account} is now offline!"
     fi
   done
+}
+
+get_participation_key_id_from_vote_key() {
+  local vote_key
+  local output
+  vote_key=$1
+
+  output=$(execute_docker_command "/node/bin/goal account partkeyinfo")
+  echo "${output}" | grep -B 10 "${vote_key}" | grep 'Participation ID:' | head -n 1 | cut -d ':' -f2- | xargs
+}
+
+get_most_recent_participation_key_details() {
+  local address=$1
+  local output
+
+  output=$(execute_docker_command "/node/bin/goal account partkeyinfo")
+
+  local first_round
+  first_round=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'First round:' | head -n 1 | cut -d ':' -f2- | xargs)
+  local last_round
+  last_round=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'Last round:' | head -n 1 | cut -d ':' -f2- | xargs)
+  local key_dilution
+  key_dilution=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'Key dilution:' | head -n 1 | cut -d ':' -f2- | xargs)
+  local selection_key
+  selection_key=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'Selection key:' | head -n 1 | cut -d ':' -f2- | xargs)
+  local voting_key
+  voting_key=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'Voting key:' | head -n 1 | cut -d ':' -f2- | xargs)
+  local stateproof_key
+  # shellcheck disable=SC2034
+  stateproof_key=$(echo "$output" | grep -A 4 -B 5 "${latest_key_end_block}" | grep 'State proof key:' | head -n 1 | cut -d ':' -f2- | xargs)
+
+  key_info_json=$(jq --null-input --arg address "$address" --arg first_round "$first_round" --arg last_round "$last_round" --arg key_dilution "$key_dilution" --arg selection_key "$selection_key" --arg voting_key "$voting_key" --arg stateproof_key "$stateproof_key" '{
+    "address": $address,
+    "first_round": $first_round,
+    "last_valid": $last_round,
+    "key_dilution": $key_dilution,
+    "selection_key": $selection_key,
+    "voting_key": $voting_key,
+    "stateproof_key": $stateproof_key
+  }')
+
+  echo "${key_info_json}"
 }
 
 generate_participation_key() {
@@ -770,48 +814,110 @@ check_staking_accounts() {
       local json_response
 
       staking_endpoint="${staking_url}?owner=${account}&deleted=0"
-      echo "${staking_endpoint}"
       response=$(curl -s --max-time 5 -w "%{http_code}" "${staking_endpoint}")
       http_code=$(echo "${response}" | awk '{print substr($0, length($0) - 2)}')
       json_response=$(echo "${response}" | awk '{print substr($0, 1, length($0) - 3)}')
 
-      echo "http code: ${http_code}"
-      echo "json response: ${json_response}"
-
       if [[ "${http_code}" -eq 200 ]]; then
         accounts_length=$(jq '.accounts | length' <<< "${json_response}")
         if [[ "${accounts_length}" -gt 0 ]]; then
-          echo "Account ${account} has received an airdrop / is a staking account."
+          display_banner "Staking contract detected"
+
+          echo "Staking contract has been detected for owner ${account}"
+          echo ""
+
+          local balance
+          balance=$(get_account_balance "${account_addr}")
+
+          if [[ ${balance} -lt "1000" ]]; then
+            echo "Balance is below 1,000 microVoi. Skipping staking account setup."
+            continue
+          fi
+
           local staking_accounts
           staking_accounts=$(jq -c '.accounts[]' <<< "${json_response}")
           for staking_account in ${staking_accounts}; do
+            local last_committed_block
             local contract_address
-            # local contract_id
+            local contract_id
             local part_vote_k
+            local part_vote_lst
+            local participation_key_id
 
-            # contract_id=$(jq -r '.contractId' <<< "${staking_account}")
+            contract_id=$(jq -r '.contractId' <<< "${staking_account}")
             contract_address=$(jq -r '.contractAddress' <<< "${staking_account}")
-            part_vote_k=$(jq -r '.part_vote_k' <<< "${staking_account}")
+            last_committed_block=$(get_last_committed_block)
 
-            echo "Processing account ID: ${contract_address}"
-            if [ "${part_vote_k}" == "null" ]; then
-              echo "Part key is null, generating a new participation key..."
+            part_vote_k=$(jq -r '.part_vote_k' <<< "${staking_account}")
+            part_vote_lst=$(jq -r '.part_vote_lst' <<< "${staking_account}")
+
+            # shellcheck disable=SC2046
+            participation_key_id=$(get_participation_key_id_from_vote_key "${part_vote_k}")
+
+            if [[ "${part_vote_k}" == "null" || $((part_vote_lst-last_committed_block)) -le 41710400 || -z "${participation_key_id}" ]]; then
+
+              if  [[ "${part_vote_k}" == "null" ]]; then
+                echo "No staking participation key detected."
+              elif [[ -z ${participation_key_id} ]]; then
+                echo "Registered staking participation key is not installed locally."
+              else
+                local existing_expiration_date
+                existing_expiration_date=$(get_participation_expiration_eta "${part_vote_lst}" "${last_committed_block}")
+
+                echo "Current participation key for account ${account} is expected to expire at: ${existing_expiration_date}"
+                echo "Currently the network is at block: ${last_committed_block}"
+                echo "Current participation key expires at block: ${part_vote_lst}"
+                echo ""
+                echo "This is below the required threshold of 417,104 blocks / ~14 days."
+              fi
+
+              echo ""
+              echo "Generating new key for owner ${account} and contract address ${contract_address}"
+
               generate_new_key "${contract_address}"
 
+              local partkey_info
+              local voting_key
+              local selection_key
+              local first_round
+              local last_round
+              local key_dilution
+              local stateproof_key
 
+              partkey_info=$(get_most_recent_participation_key_details "${contract_address}")
+              voting_key=$(jq -r '.voting_key' <<< "${partkey_info}")
+              selection_key=$(jq -r '.selection_key' <<< "${partkey_info}")
+              first_round=$(jq -r '.first_round' <<< "${partkey_info}")
+              last_round=$(jq -r '.last_valid' <<< "${partkey_info}")
+              key_dilution=$(jq -r '.key_dilution' <<< "${partkey_info}")
+              stateproof_key=$(jq -r '.stateproof_key' <<< "${partkey_info}")
 
               execute_interactive_docker_command "/node/bin/goal clerk send -a 1000 -f ${account} -t ${contract_address} --out=/tmp/payment.txn"
-              # execute_interactive_docker_command "/node/bin/goal app call --app-id ${contract_id} --from ${account} --app-arg 'b64:zSTeiA==' --app-arg 'b64:${voting_key}' --app-arg 'b64:${selection_key}' --app-arg 'int:${first_round}' --app-arg 'int:${last_round}' --app-arg 'int:${key_dilution}' --app-arg 'b64:${stateproof_key}' --out=/tmp/app_call.txn"
+              execute_interactive_docker_command "/node/bin/goal app call --app-id ${contract_id} --from ${account} --app-arg 'b64:zSTeiA==' --app-arg 'b64:${voting_key}' --app-arg 'b64:${selection_key}' --app-arg 'int:${first_round}' --app-arg 'int:${last_round}' --app-arg 'int:${key_dilution}' --app-arg 'b64:${stateproof_key}' --out=/tmp/app_call.txn"
 
+              execute_docker_command "cat /tmp/{payment,app_call}.txn > /tmp/combined.txn"
+              execute_interactive_docker_command "/node/bin/goal clerk group -i /tmp/combined.txn -o /tmp/grouped.txn > /dev/null"
+              execute_interactive_docker_command "/node/bin/goal clerk split -i /tmp/grouped.txn -o /tmp/split.txn > /dev/null"
+              execute_interactive_docker_command "/node/bin/goal clerk sign -i /tmp/split-0.txn -o /tmp/signed-0.txn"
+              execute_interactive_docker_command "/node/bin/goal clerk sign -i /tmp/split-1.txn -o /tmp/signed-1.txn"
+              execute_docker_command "cat /tmp/signed-{0,1}.txn > /tmp/signed-combined.txn"
+              execute_interactive_docker_command "/node/bin/goal clerk rawsend -f /tmp/signed-combined.txn"
+
+              if [[ -n ${participation_key_id} ]]; then
+                execute_interactive_docker_command "/node/bin/goal account deletepartkey --partkeyid ${participation_key_id}"
+              fi
             else
-              echo "Part key: ${part_vote_k}"
+              local existing_expiration_date
+              existing_expiration_date=$(get_participation_expiration_eta "${part_vote_lst}" "${last_committed_block}")
+
+              echo "Current staking participation key for owner ${account}, and"
+              echo "contract address ${contract_address} is expected to expire at: ${existing_expiration_date}"
+              echo "This is above the required threshold of 417,104 blocks / ~14 days."
+              echo "No new staking participation key will be generated."
+              echo ""
             fi
           done
-        else
-          echo "Account ${account} has not received an airdrop / is not a staking account."
         fi
-      else
-        echo "Failed to fetch staking information for account ${account}."
       fi
         # Format of object is:
 #            {
@@ -843,11 +949,6 @@ check_staking_accounts() {
 #              "deleted": 0
 #            }
 
-        # if vote_key matches a local key, and part_vote_lst is within 417,104 blocks of the last committed block, renew the key, if the accout has a balance of 1,000 microVoi or more.
-        # if new keys are generated, delete the old key.
-        # after keys have been generated create an offline payment transaction
-        # after having generated an offline payment transaction, create a new inner app transaction on contractAddress from the json object.
-        # sign both transactions as a group, in the order of creation, and submit to the network.
         # after this has been done for all accounts, set a state variable to indicate that the accounts have been processed.
     done
   fi
